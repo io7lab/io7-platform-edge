ff30d56f6be00ba0eed45f9b7e61ebc5d6faaf89
Yoonseok Hur <yoonseok@gmail.com>
2023-06-23 15:28:26 +0900

    docker arm/intel

--- setup/Setup_Runtime.sh
+++ setup/Setup_Runtime.sh
@@ -3,13 +3,29 @@
 # This script installs and sets the Runtime environment of the HomeOS
 #
 
+if [ $(uname) = 'Linux' ]
+then
+    if [ $(uname -m) = 'x86_64' ]
+    then
+        nodejs_url='https://nodejs.org/dist/v18.16.1/node-v18.16.1-linux-x64.tar.xz'
+    else
+        nodejs_url='https://nodejs.org/dist/v18.16.0/node-v18.16.0-linux-arm64.tar.xz'
+    fi
+else
+    echo "This script is for Linux only"
+    echo "If you are using Windows or Mac, then install and configure the following"
+    echo -e "\t1. nodejs"
+    echo -e "\t2. docker"
+    echo -e "\t3. docker-compose"
+    exit 1
+fi
+
 echo "nodejs will be installed for the link =>"
 echo "        https://nodejs.org/dist/v18.16.0/node-v18.16.0-linux-arm64.tar.xz"
 echo "if you you want to install a different version or the latest if any"
 echo "then visit https://nodejs.org/en/download/ and get the link to the nodejs tarball"
 echo "paste the link here, otherwise just hit enter"
 
-nodejs_url='https://nodejs.org/dist/v18.16.0/node-v18.16.0-linux-arm64.tar.xz'
 read url
 if [ -z "$url" ]
 then
