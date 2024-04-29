fb4815b8cf5ccff6646075424a23e8b30ad48046
Yoonseok Hur <yoonseok@gmail.com>
2024-04-29 23:20:26 +0900

    gateway 0.0.2

--- setup/setup_docker_nodejs.sh
+++ setup/setup_docker_nodejs.sh
@@ -2,21 +2,38 @@
 #
 # This script installs and sets the Runtime environment of the HomeOS
 #
-
+echo set tabstop=4 > ~/.vimrc
+echo set shiftwidth=4 >> ~/.vimrc
+echo set expandtab >> ~/.vimrc
+# timezone and ssh related
+sed 's/#UseDNS/UseDNS/' /etc/ssh/sshd_config | sudo tee /etc/ssh/sshd_config
+sed 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config | sudo tee /etc/ssh/sshd_config
+grep 'set -o vi' ~/.bashrc > /dev/null || echo 'set -o vi' >> ~/.bashrc
+echo export NODE_PATH=/usr/local/lib/node_modules:\$NODE_PATH >> ~/.bashrc
+#sudo apt update && sudo apt upgrade -y
+echo Enter the hostname for the machine
+read newName
+if [  "$newName" ]
+then
+    sudo hostname $newName
+    echo $newName | sudo tee /etc/hostname
+    echo logout and login again to have the new PS1 in effect
+fi
+# end of environment setup
+# start of docker and nodejs setup
 if [ $(uname) = 'Linux' ]
 then
     if [ $(uname -m) = 'x86_64' ]
     then
-        nodejs_url='https://nodejs.org/dist/v18.16.1/node-v18.16.1-linux-x64.tar.xz'
+        nodejs_url='https://nodejs.org/dist/v20.11.1/node-v20.11.1-linux-x64.tar.xz'
     else
-        nodejs_url='https://nodejs.org/dist/v18.16.0/node-v18.16.0-linux-arm64.tar.xz'
+        nodejs_url='https://nodejs.org/dist/v20.11.1/node-v20.11.1-linux-arm64.tar.xz'
     fi
 else
     echo "This script is for Linux only"
     echo "If you are using Windows or Mac, then install and configure the following"
-    echo -e "\t1. nodejs"
+    echo -e "\t1. nodejs and npm"
     echo -e "\t2. docker"
-    echo -e "\t3. docker-compose"
     exit 1
 fi
 
@@ -43,12 +60,11 @@ then
     sudo cp -R * /usr/local
     cd -
     rm -rf $(echo $fname|sed 's/.tar.xz$//' | sed 's/.tar.gz$//')
-    sudo npm -g i mqtt basic-auth body-parser cron-parser 
+    sudo npm -g i mqtt basic-auth body-parser cron-parser yaml
     sudo npm -g i express fs loader node-schedule redis request
 else
     echo check the url for the nodejs
 fi
-
 
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 id pi 2> /dev/null > /dev/null
@@ -58,7 +74,7 @@ else
     sudo usermod -aG docker ubuntu
 fi
 sudo systemctl restart containerd
-sudo systemctl restart docker.service docker.socket
+sudo systemctl restart docker.service
 sudo apt install docker-compose -y
 
 echo if you are using the serial port device, you need to enable the serial port
