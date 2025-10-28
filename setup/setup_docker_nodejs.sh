31a1af552987fa9e77bfc5e05eb1811c0badeca0
Yoonseok Hur <yoonseok@gmail.com>
2025-10-28 22:22:03 +0900

    nodejs update & get-docker.sh update

--- setup/setup_docker_nodejs.sh
+++ setup/setup_docker_nodejs.sh
@@ -25,9 +25,9 @@ if [ $(uname) = 'Linux' ]
 then
     if [ $(uname -m) = 'x86_64' ]
     then
-        nodejs_url='https://nodejs.org/dist/v20.11.1/node-v20.11.1-linux-x64.tar.xz'
+        nodejs_url='https://nodejs.org/dist/v24.10.0/node-v24.10.0-linux-x64.tar.xz'
     else
-        nodejs_url='https://nodejs.org/dist/v20.11.1/node-v20.11.1-linux-arm64.tar.xz'
+        nodejs_url='https://nodejs.org/dist/v24.10.0/node-v24.10.0-linux-arm64.tar.xz'
     fi
 else
     echo "This script is for Linux only"
@@ -66,7 +66,7 @@ else
     echo check the url for the nodejs
 fi
 
-curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
 id pi 2> /dev/null > /dev/null
 if [ $? -eq 0 ] ; then
     sudo usermod -aG docker pi
